a0dd3a3946f8fc769c46854998a6c232504fe3b2
raohonghua <raohonghua@foxmail.com>
2017-12-06 20:35:22 +0800

    add user component

--- src/components/user/user.vue
+++ src/components/user/user.vue
@@ -1,5 +1,17 @@
 <template>
-  <div class="user"></div>
+  <transition name="slide">
+    <div class="user">
+      <div class="back">
+        <i class="icon-back"></i>
+      </div>
+      <div class="switches-wrapper"></div>
+      <div class="play-btn" ref="playBtn">
+        <i class="icon-play"></i>
+        <span class="text">随机播放全部</span>
+      </div>
+      <div class="list-wrapper" ref="listWrapper"></div>
+    </div>
+  </transition>
 </template>
 
 <script>
