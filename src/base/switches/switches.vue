061072ad25e34433a7af4e140b71a85a67a5a626
raohonghua <raohonghua@foxmail.com>
2018-04-16 19:11:02 +0800

    add playlist module

---
+++ src/base/switches/switches.vue
@@ -0,0 +1,50 @@
+<template>
+  <ul class="switches">
+    <li class="switch-item" v-for="(item,index) in switches" :class="{'active':currentIndex === index}"
+        @click="switchItem(index)">
+      <span>{{item.name}} </span>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'switches',
+  props: {
+    switches: {
+      type: Array,
+      default: []
+    },
+    currentIndex: {
+      type: Number,
+      default: 0
+    }
+  },
+  methods: {
+    switchItem(index) {
+      this.$emit('switch', index);
+    }
+  }
+}
+</script>
+
+<style scoped lang="stylus">
+@import "../../common/stylus/variable"
+
+.switches
+  display: flex
+  align-items: center
+  width: 240px
+  margin: 0 auto
+  border: 1px solid #aaaaaa
+  border-radius: 5px
+  .switch-item
+    flex: 1
+    padding: 8px
+    text-align: center
+    font-size: $font-size-medium
+    color: #aaaaaa
+    &.active
+      background: #222222
+      color: $color-text
+</style>
---
+++ src/components/add-song/add-song.vue
@@ -0,0 +1,185 @@
+<template>
+  <transition name="slide">
+    <div class="add-song" v-show="showFlag" @click.stop>
+      <div class="header">
+        <h1 class="title">添加歌曲到列表</h1>
+        <div class="close" @click="hide">
+          <i class="icon-close"></i>
+        </div>
+      </div>
+      <div class="search-box-wrapper">
+        <v-search-box ref="searchBox" @query="onQueryChange" placeholder="搜索歌曲"></v-search-box>
+      </div>
+      <div class="shortcut" v-show="!query">
+        <v-switches :switches="switches" :currentIndex="currentIndex" @switch="switchItem"></v-switches>
+        <div class="list-wrapper">
+          <v-scroll ref="songList" v-if="currentIndex===0" class="list-scroll" :data="playHistory">
+            <div class="list-inner">
+              <v-song-list :songs="playHistory" @select="selectSong"></v-song-list>
+            </div>
+          </v-scroll>
+          <v-scroll :refreshDelay="refreshDelay" ref="searchList" v-if="currentIndex===1" class="list-scroll" :data="searchHistory">
+            <div class="list-inner">
+              <v-search-list @delete="deleteSearchHistory" @select="addQuery" :searches="searchHistory"></v-search-list>
+            </div>
+          </v-scroll>
+        </div>
+      </div>
+      <div class="search-result" v-show="query">
+        <v-suggest :query="query" :showSinger="showSinger" @select="selectSuggest" @listScroll="blurInput"></v-suggest>
+      </div>
+      <v-top-tip ref="topTip">
+        <div class="tip-title">
+          <i class="icon-ok"></i>
+          <span class="text">1首歌曲已经添加到播放列表</span>
+        </div>
+      </v-top-tip>
+    </div>
+  </transition>
+</template>
+
+<script>
+import vSearchBox from '@/base/search-box/search-box'
+import vSongList from '@/base/song-list/song-list'
+import vSearchList from '@/base/search-list/search-list'
+import vScroll from '@/base/scroll/scroll'
+import vSwitches from '@/base/switches/switches'
+import vTopTip from '@/base/top-tip/top-tip'
+import vSuggest from '@/components/suggest/suggest'
+import {searchMixin} from '@/common/js/mixin'
+import {mapGetters, mapActions} from 'vuex'
+import Song from '@/common/js/song'
+
+export default {
+  name: 'add-song',
+  components: {
+    vSearchBox,
+    vSongList,
+    vSearchList,
+    vScroll,
+    vSwitches,
+    vTopTip,
+    vSuggest
+  },
+  mixins: [searchMixin],
+  data() {
+    return {
+      showFlag: false,
+      showSinger: false,
+      currentIndex: 0,
+      songs: [],
+      switches: [
+        {
+          name: '最近播放'
+        },
+        {
+          name: '搜索历史'
+        }
+      ]
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'playHistory'
+    ])
+  },
+  methods: {
+    show() {
+      this.showFlag = true;
+      setTimeout(() => {
+        if (this.currentIndex === 0) {
+          this.$refs.songList.refresh();
+        } else {
+          this.$refs.searchList.refresh();
+        }
+      }, 20);
+    },
+    hide() {
+      this.showFlag = false;
+    },
+    selectSong(song, index) {
+      if (index !== 0) {
+        this.insertSong(new Song(song));
+        this.$refs.topTip.show();
+      }
+    },
+    selectSuggest() {
+      this.$refs.topTip.show();
+      this.saveSearch();
+    },
+    switchItem(index) {
+      this.currentIndex = index;
+    },
+    ...mapActions([
+      'insertSong'
+    ])
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import "../../common/stylus/variable"
+@import "../../common/stylus/mixin"
+
+.add-song
+  position: fixed
+  top: 0
+  bottom: 0
+  width: 100%
+  z-index: 200
+  background: #333333
+  &.slide-enter-active, &.slide-leave-active
+    transition: all 0.3s
+  &.slide-enter, &.slide-leave-to
+    transform: translate3d(100%, 0, 0)
+  .header
+    position: relative
+    height: 44px
+    text-align: center
+    .title
+      line-height: 44px
+      font-size: $font-size-large
+      color: #aaaaaa
+    .close
+      position: absolute
+      top: 0
+      right: 8px
+      .icon-close
+        display: block
+        padding: 12px
+        font-size: 20px
+        color: $color-theme
+
+  .search-box-wrapper
+    margin: 20px
+  .shortcut
+    .list-wrapper
+      position: absolute
+      top: 165px
+      bottom: 0
+      width: 100%
+      .list-scroll
+        height: 100%
+        overflow: hidden
+        .list-inner
+          padding: 20px 30px
+  .search-result
+    position: fixed
+    top: 124px
+    bottom: 0
+    width: 100%
+  .tip-title
+    text-align: center
+    padding: 18px 0
+    font-size: 0
+    .icon-ok
+      font-size: $font-size-medium
+      color: $color-theme
+      margin-right: 4px
+    .text
+      font-size: $font-size-medium
+      color: #aaaaaa
+
+</style>
+
+
